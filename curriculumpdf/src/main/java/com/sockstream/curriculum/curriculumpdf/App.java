f59ac862a66a6a9f54bd474dd44aac3ab5c58f69
sockstream <clement.beze@gmail.com>
2018-04-19 23:48:29 +0200

    dependency added, gitignore updated

--- curriculumpdf/src/main/java/com/sockstream/curriculum/curriculumpdf/App.java
+++ curriculumpdf/src/main/java/com/sockstream/curriculum/curriculumpdf/App.java
@@ -1,13 +1,30 @@
 package com.sockstream.curriculum.curriculumpdf;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private static Logger sLOGGER = LogManager.getLogger(App.class);
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        sLOGGER.info("Starting");
+        
+       //handle outputfilePath in parameter
+        
+        //handle linkedin url in parameter
+        
+        //retrieve document from url
+        
+        //parse document to forge pdf
+        
+        //export pdf
+        
+        sLOGGER.info("Stop");
     }
 }
